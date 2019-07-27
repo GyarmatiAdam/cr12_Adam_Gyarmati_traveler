29fb2ffceff5bed3e30875d92794c0b6cb0e0099
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-07-27 09:35:50 +0200

    older version

--- content.php
+++ content.php
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 <div class="shadow-lg p-3 mb-5 bg-white rounded post">
 <h2><?php the_title(); ?></h2>
 <p><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 <?php the_content();?> 
 </div><br><br>
+=======
+
+	<h2><?php the_title(); ?></h2>
+	<p><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+
+ <?php the_content(); ?>
+>>>>>>> parent of a83d0ed... all required function works
--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
     <div class="col-sm-3">
       <div class="sidebar-module">
         <h4>About</h4>
@@ -18,6 +19,18 @@
         <?php endif; ?>
       </div>
     </div>
+=======
+<div class="col-sm-3">
+  <div class="sidebar-module">
+    <h4>About</h4>
+    <p><?php the_author_meta( 'description' ); ?> </p>
+  </div>
+  <div class="sidebar-module">
+    <h4>Archives</h4>
+    <ol>
+        <?php wp_get_archives( 'type=monthly' ); ?>
+    </ol>
+>>>>>>> parent of a83d0ed... all required function works
   </div>
 </div>
 <!-- /.blog-sidebar -->
